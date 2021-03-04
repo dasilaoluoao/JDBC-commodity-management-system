94d52866f94876c0aa53672c664f6da274608c85
siqi Zhang <49664983+dasilaoluoao@users.noreply.github.com>
2021-03-04 23:24:32 +0800

    Add files via upload

---
+++ Mysqlwork.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mysqlwork;
+
+import java.sql.*;
+
+/**
+ *
+ * @author admin
+ */
+public class Mysqlwork {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.lang.ClassNotFoundException
+     * @throws java.sql.SQLException
+     */
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        // TODO code application logic here
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                p1.getPp().setVisible(true);
+            }
+        });
+    }
+
+}
